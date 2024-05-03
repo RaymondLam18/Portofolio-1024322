7cd6cd2478c72cfacae464170b97e6b701b8ad1e
RaymondLam18 <1024322@hr.nl>
2024-05-03 11:40:09 +0200

    update
    programmeren 8 toegevoegd

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const project2Images = document.querySelectorAll('.CLE2project');
     const project3Images = document.querySelectorAll('.CLE3project');
     const project4Images = document.querySelectorAll('.CLE4project');
+    const programmeren8Images = document.querySelectorAll('.programmeren8');
 
     function addClickListenerToImages(images) {
         images.forEach(image => {
@@ -24,6 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
     addClickListenerToImages(project2Images);
     addClickListenerToImages(project3Images);
     addClickListenerToImages(project4Images);
+    addClickListenerToImages(programmeren8Images);
 
     const closeBtn = document.querySelector('.close');
     closeBtn.addEventListener('click', function() {
